ce13ae15815ccacc565db447ae6765c1c3eafe48
cadarsh440 <129061051+cadarsh440@users.noreply.github.com>
2023-07-29 11:41:08 +0530

    Update products_controller.js

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,71 +1,116 @@
 const Product = require('../models/product');
 
-// function to show all the products
-module.exports.products = function(req, res){
-    Product.find({}, function(err, foundProducts){
-        if(err){
-            res.send(err);
-        }else{
-            res.send(foundProducts);
-        }
-    });
-}
-
-// function to create a new product
-module.exports.create = function(req, res){
-    const newProduct = new Product({
-        name: req.body.name,
-        quantity: req.body.quantity
-    });
-    newProduct.save(function(err){
-        if(err){
-            res.send(err);
-        }else{
-            res.send('New product added successfully.');
-        }
-    });
-}
-
-// function to delete a product using it's ID
-module.exports.delete = function(req, res){
-    Product.deleteOne(
-        {_id: req.params.productID},
-        function(err){
-            if(err){
-                res.send(err);
-            }else{
-                res.send({
-                    message: "Product deleted"
-                });
-            }
-        });
-}
-
-// function to update a product's quantity
-module.exports.updateQunatity = function(req, res){
-    const ID = req.params.productID;
-    // find the product using id
-    Product.findById(ID, function(err, found){
-        if(err){
-            res.send(err);
-        }else{
-
-            // Note - To increment the quantity of the product put number as a positive value in the query 
-            //        and to decrement the quantity put the number as negative value in the query
-
-            const newQty = parseInt(found.quantity) + parseInt(req.query.number);
-            // update the product's quantity
-            Product.findByIdAndUpdate(ID, {quantity: newQty}, function(err, updatedProduct){
-                if(err){
-                    res.send(err);
-                }else{
-                    updatedProduct.quantity = newQty;
-                    res.send({
-                        product: updatedProduct,
-                        message: 'updated successfully'
-                    });
-                }
+// to get all the products
+module.exports.products = async (req, res) => {
+    try {
+        const products = await Product.find()
+        if (products.length === 0) {
+            return res.status(400).json({
+                message: "No products found"
             });
         }
-    });
+        return res.status(200).json({
+            data: {
+                products: products
+            },
+            message: "All products"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Error in fetching the products"
+        });
+    }
+};
+
+// for creating the product
+module.exports.create = async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.quantity) {
+            return res.status(400).json({
+                message: "please enter name and quantity"
+            });
+        }
+        const newProduct = await Product.create(req.body);
+        return res.status(201).json({
+            data: {
+                product: newProduct
+            },
+            message: "Product Created successfully"
+        })
+    } catch (err) {
+        return res.status(500).json({
+            data: {
+                err: err
+            },
+            message: "Error in Creating the product"
+        });
+    }
+};
+
+// for deleting the product
+module.exports.delete = async (req, res) => {
+    try {
+        const productId = await req.params.id;
+        const product = await Product.findByIdAndDelete(productId);
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+        return res.status(200).json({
+            message: "deleted successfully"
+        })
+    } catch (err) {
+        return res.status(500).json({
+            data: {
+                err: err
+            },
+            message: "Error in deleting the product"
+        });
+    }
 }
+
+// for updating quantity of product
+module.exports.update = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const quantity = req.query.number;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                message: "Invalid product ID"
+            });
+        }
+
+        if (!quantity || isNaN(quantity)) {
+            return res.status(400).json({
+                message: "Invalid quantity value"
+            });
+        }
+
+        const product = await Product.findById(id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        product.quantity = quantity;
+        const updatedProduct = await product.save();
+
+        return res.status(200).json({
+            data: {
+                product: updatedProduct
+            },
+            message: "Updated successfully"
+        });
+    } catch (err) {
+        return res.status(500).json({
+            data: {
+                err: err
+            },
+            message: "Error in updating the quantity"
+        });
+    }
+};
