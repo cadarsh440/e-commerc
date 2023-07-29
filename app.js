9987e85e1aa558499d99f32b3bfab8cf5f6dc771
cadarsh440 <129061051+cadarsh440@users.noreply.github.com>
2023-07-29 11:39:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-// importing required packages
+// Import the Express module
 const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./config/mongoose');
 
-// initializing express
+// Set the port number
+const port = process.env.PORT || 8000;
+
+// Create an instance of the Express application
 const app = express();
 
-// using body parser to parse over the request body
-app.use(bodyParser.urlencoded({extended: true}));
+const env = require('dotenv').config();
 
-// using routes
-app.use('/products', require('./routes/products'));
+// middleware's for parsing data in database
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-// starting the server
-app.listen(3000, function(){
-    console.log('API is live on http://localhost:3000/products');
+// Import the Mongoose module for database connection
+const db = require('./config/mongoose');
+
+// Import the router for product-related routes
+const productRouter = require('./routers/product');
+
+// Use the product router for all routes starting with '/'
+app.use('/products', productRouter);
+
+// Start the server and listen on the specified port
+app.listen(port, (err) => {
+    if (err) {
+        // Print an error message if the server fails to start
+        console.log(`Error in connecting to the server: ${err}`);
+    }
+    // Print a success message if the server starts successfully
+    console.log(`Server is up and running successfully on port: ${port}`);
 });
